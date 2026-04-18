bb2a8f05092126d3356917e7fc6fbb4b4e882a21
humayun-dev6 <humayunzahid234@gmail.com>
2026-04-18 11:13:01 -0700

    Update hello.py
    "Completed Library and Book class"

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
 class Library:
-    def __init__(self,item):
+    def __init__(self, item):
         self.item = item
 
-        def welcome(self):
-            print(f"Welcome to {self.item} library!")
+    def welcome(self):
+        print(f"Welcome to {self.item} library !")
 
 class Book(Library):
-    def __init__(self, item, title, author) 
-      super().__init__(item)
-      self.author = author
-      self.title = title
+    def __init__(self, item, title, author):
+        super().__init__(item)
+        self.author = author
+        self.title = title
+
+# Code ko chalane ke liye:
+my_book = Book("Punjab University", "Python Guide", "Dr Ahmad")
+my_book.welcome()
+print(f"Book Name:{my_book.title}")
+print(f"Author:{my_book.author}")

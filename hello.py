ce772da634fc40ea18f61f1eaec9ee16f5aba1ef
humayun-dev6 <humayunzahid234@gmail.com>
2026-04-18 10:27:07 -0700

    Create hello.py
    Fixed super() syntax in Book class.

---
+++ hello.py
@@ -0,0 +1,12 @@
+class Library:
+    def __init__(self,item):
+        self.item = item
+
+        def welcome(self):
+            print(f"Welcome to {self.item} library!")
+
+class Book(Library):
+    def __init__(self, item, title, author) 
+      super().__init__(item)
+      self.author = author
+      self.title = title
